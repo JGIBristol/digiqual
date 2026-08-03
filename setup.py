24b3590d216fb9f26f50177db9e6a96b24da064e
Josh Tyler <55984880+JTyler13@users.noreply.github.com>
2026-08-03 11:51:31 +0100

    add pybind11 runtime dependency and ruff as dev tool

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
-import sys
-import os
-from setuptools import setup, find_packages
 from pybind11.setup_helpers import Pybind11Extension, build_ext
+from setuptools import find_packages, setup
+
 
 class CustomBuildExt(build_ext):
     def build_extensions(self):
         for ext in self.extensions:
             compiler_type = self.compiler.compiler_type
-            if compiler_type == 'msvc':
-                ext.extra_compile_args.extend(['/O2', '/std:c++17'])
+            if compiler_type == "msvc":
+                ext.extra_compile_args.extend(["/O2", "/std:c++17"])
             else:
-                ext.extra_compile_args.extend(['-O3', '-std=c++17'])
+                ext.extra_compile_args.extend(["-O3", "-std=c++17"])
 
         super().build_extensions()
+
 
 ext_modules = [
     Pybind11Extension(
